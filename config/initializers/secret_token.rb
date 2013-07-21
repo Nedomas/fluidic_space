e108e12dc61a4a686428b93b3c2764a5ede603ab
Domas <domas.bitvinskas@me.com>
2013-07-22 01:55:40 +0300

    Initial empty app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+FluidicSpace::Application.config.secret_key_base = 'a3f2e21c2ff143112827e45e04aa0a7736fb8e7b142390cc6e50892b02323881ea1c6d38abb11a73a9390fe3d6150a493c5383670422e3486b349b453d99aea2'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+FluidicSpace::Application.config.session_store :cookie_store, key: '_fluidic_space_session'
